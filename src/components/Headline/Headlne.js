95aa1d16511390086e0efa51aa654b4b0966119c
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-11 23:56:04 +0600

    bootstrap button added

--- src/components/Headline/Headlne.js
+++ src/components/Headline/Headlne.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Headline.css';
+import { Button } from 'react-bootstrap';
 
 const Headlne = () => {
     return (
         <div>
             <h1>Headline page</h1>
+            <Button variant="primary">Primary</Button>{' '}
         </div>
     );
 };
