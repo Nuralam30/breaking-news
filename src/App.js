329e471318aa5d0339a3e38b1f38b9976b0d7ab3
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-12 00:01:01 +0600

    bootstrap card added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Headline from './components/Headline/Headlne';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Header.css';
+import { Card } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <h2>Hello </h2>
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+                </Card.Text>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+            </Card>
         </div>
     );
 };
