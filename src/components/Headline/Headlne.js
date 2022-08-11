0b6aed82bfd217507e3c26a698e4e93575486ed0
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-11 22:08:43 +0600

    header and Headline components created

---
+++ src/components/Headline/Headlne.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Headline.css';
+
+const Headlne = () => {
+    return (
+        <div>
+            <h1>Headline page</h1>
+        </div>
+    );
+};
+
+export default Headlne;
